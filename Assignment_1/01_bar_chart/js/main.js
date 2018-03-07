cddfcdf8aca3d44a2dd4b4dc5b30514a9b486b4b
khirani6 <khirani6@gatech.edu>
2018-03-07 15:40:22 -0500

    Adding files and loading csv file in d3

---
+++ Assignment_1/01_bar_chart/js/main.js
@@ -0,0 +1,13 @@
+
+// **** Your JavaScript code goes here ****
+// Name: Komal Hirani
+// gtID: khirani6
+// Coding Assignment #1
+// Copyrighted; Do not use this code unless you specifically ask for permission
+
+//Loading csv file:
+d3.csv('./data/coffee_data.csv', function(error, data) {
+	console.log(data);
+
+});
+
